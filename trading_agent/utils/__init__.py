96d435d78e8a499026c73edcfc45102483e0e5b5
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2026-06-16 11:15:38 +0200

    Update utils __init__

--- trading_agent/utils/__init__.py
+++ trading_agent/utils/__init__.py
@@ -1,16 +1,17 @@
-from trading_agent.utils.config import require_env
-from trading_agent.utils.logger import configure_logging, get_logger
-from trading_agent.utils.http import request_json
+from trading_agent.utils.http import request_json, require_env
 from trading_agent.utils.llm_clients import FallbackLlmClient, OllamaJsonClient, OpenAiJsonClient
 from trading_agent.utils.llm_metadata import llm_metadata
+from trading_agent.utils.logger import configure_logging, get_logger
+from trading_agent.utils.portfolio import safe_portfolio_snapshot
 
 __all__ = [
-    "require_env",
-    "get_logger",
-    "configure_logging",
-    "request_json",
     "FallbackLlmClient",
     "OllamaJsonClient",
     "OpenAiJsonClient",
+    "configure_logging",
+    "get_logger",
     "llm_metadata",
+    "request_json",
+    "require_env",
+    "safe_portfolio_snapshot",
 ]
