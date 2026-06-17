83eaeedc9fc0d629de3c47f39fc5c9b7f864bd3c
BrunoCarusoc <brunocarusoc@gmail.com>
2026-06-17 08:16:35 +0200

    fix: update utils module init

--- trading_agent/utils/__init__.py
+++ trading_agent/utils/__init__.py
@@ -1,5 +1,5 @@
 from trading_agent.utils.config import require_env
-from trading_agent.utils.http import request_json
+from trading_agent.utils.http_requests import request_json
 from trading_agent.utils.llm_clients import FallbackLlmClient, OllamaJsonClient, OpenAiJsonClient, OpenRouterJsonClient
 from trading_agent.utils.llm_metadata import llm_metadata
 from trading_agent.utils.logger import configure_logging, get_logger
